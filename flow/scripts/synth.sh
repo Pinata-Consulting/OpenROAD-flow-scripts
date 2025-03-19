2a68e5f14f82ed4878aedde520fb265cf6421172
Øyvind Harboe <oyvind.harboe@zylin.com>
2025-03-19 20:37:39 +0100

    makefile: move code out of make into bash
    A step in the right direction to getting rid of makefile dependency
    
    Signed-off-by: Øyvind Harboe <oyvind.harboe@zylin.com>

---
+++ flow/scripts/synth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -u -eo pipefail
+mkdir -p $RESULTS_DIR $LOG_DIR $REPORTS_DIR $OBJECTS_DIR
+
+export YOSYS_FLAGS="${YOSYS_FLAGS:-} -v 3"
+
+# Not normally adjusted by user
+export SYNTH_OPERATIONS_ARGS="${SYNTH_OPERATIONS_ARGS:--extra-map ${FLOW_HOME}/platforms/common/lcu_kogge_stone.v}"
+export SYNTH_FULL_ARGS="${SYNTH_FULL_ARGS:=${SYNTH_ARGS} ${SYNTH_OPERATIONS_ARGS}}"
+
+eval "$TIME_CMD $YOSYS_EXE $YOSYS_FLAGS -c $1" 2>&1 | tee $(realpath $2)
